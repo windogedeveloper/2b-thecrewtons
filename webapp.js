b8dee047f34189b4e44a1c5609ecc609e5814bd5
Brayden Crabb <brayden.crabb@warriorlife.net>
2018-01-29 10:21:02 -0800

    Tree

--- webapp.js
+++ webapp.js
@@ -1,1 +1,15 @@
 // JavaScript Document
+
+// global variables
+var money = 0;
+var trees = 1;
+var land = 1;
+
+function crouton_click() {
+	money++;
+	document.getElementById("score").value = money;
+}
+
+document.getElementById("tree").addEventListener("click", function(){
+	crouton_click();
+});
