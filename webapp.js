9aa65fd78676111debdb603b7102d63aeaef935d
Brayden Crabb <brayden.crabb@warriorlife.net>
2018-01-31 11:54:49 -0800

    trees and score update and buy more trees works

--- webapp.js
+++ webapp.js
@@ -6,11 +6,31 @@ var trees = 1;
 var land = 1;
 
 function crouton_click() {
-	money++;
-	document.getElementById("score").value = money;
+	money += trees;
+	update_values();
 }
 
 window.onload = function() {
 	// add event listeners
 	document.getElementById("tree").addEventListener("click", crouton_click);
+	document.getElementById("moretrees").addEventListener("click", buy_trees);
 };
+
+function buy_land() {
+	update_values();
+}
+
+function buy_trees() {
+	if( money >= 50 ) {
+		money -= 50;
+		trees++;
+	}
+	update_values();
+}
+
+function update_values() {
+	document.getElementById("score").value = money;
+	document.getElementById("trees").value = trees;
+	document.getElementById("land").value = land;
+
+}
