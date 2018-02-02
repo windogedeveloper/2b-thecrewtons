24a6851b5bbd57f8ba789fe6d449575d41fe28d3
Brayden Crabb <brayden.crabb@warriorlife.net>
2018-02-02 11:18:45 -0800

    just a lot of stuff

--- webapp.js
+++ webapp.js
@@ -3,7 +3,9 @@
 // global variables
 var money = 0;
 var trees = 1;
-var land = 1;
+var land = 5;
+var tree_price = 1;
+var land_price = 10;
 
 function crouton_click() {
 	money += trees;
@@ -11,19 +13,38 @@ function crouton_click() {
 }
 
 window.onload = function() {
+	// load cookies (if they exist)
+	
+	// update values
+	update_values();
 	// add event listeners
 	document.getElementById("tree").addEventListener("click", crouton_click);
 	document.getElementById("moretrees").addEventListener("click", buy_trees);
+	document.getElementById("moreland").addEventListener("click", buy_land);
 };
 
 function buy_land() {
+	if( money >= land_price ) {
+		money -= land_price;
+		land += 5;
+		land_price *= 1.1;
+	}
 	update_values();
 }
 
 function buy_trees() {
-	if( money >= 50 ) {
-		money -= 50;
+	if( money >= tree_price && trees < land ) {
+		money -= tree_price;
 		trees++;
+	}
+	else if( trees >= land ) {
+		alert ("You do not have enough land");
+	}
+	else if( money > tree_price ) {
+		alert ("You do not have enough CashMoney");
+	}
+	else if( money > land_price ) {
+		alert ("You do not have enough CashMoney");
 	}
 	update_values();
 }
